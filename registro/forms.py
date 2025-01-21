640336eea18621ed1f8bf58aef97c54aec45224b
Gustavo GS. Said <gustavosaid@scmpm.local>
2025-01-21 13:14:10 -0300

    codigo dando errado para reconhecimento facial

---
+++ registro/forms.py
@@ -0,0 +1,48 @@
+from django import forms
+from .models import Funcionario, ColetaFaces
+
+class FuncionarioForm(forms.ModelForm):
+    class Meta:
+        model = Funcionario
+        fields = ['foto', 'nome', 'cpf']
+        widgets = {
+            'nome': forms.TextInput(attrs={'placeholder': 'Digite seu nome completo'})
+        }
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+
+
+#Multiplos arquivos
+class MultipleFileInput(forms.ClearableFileInput):
+    allow_multiple_selected = True
+
+
+class MultipleFileField(forms.FileField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", MultipleFileInput())
+        super().__init__(*args, **kwargs)
+
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial) for d in data]
+        else:
+            result = [single_file_clean(data, initial)]
+        return result
+
+
+
+class ColetaFacesForm(forms.ModelForm):
+    images = MultipleFileField()
+
+    class Meta:
+        model = ColetaFaces
+        fields = ['images']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'

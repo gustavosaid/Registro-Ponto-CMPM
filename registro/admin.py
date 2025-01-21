441b45005ae7c2ff6c6cb86a9216971899d9b5bd
Gustavo GS. Said <gustavosaid@scmpm.local>
2025-01-21 11:01:02 -0300

    Modelo pronto das fotos

---
+++ registro/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import (Funcionario, ColetaFaces, Treinamento)
+
+class ColetaFacesInline(admin.StackedInline):
+    model = ColetaFaces
+    extra = 0
+
+class FuncionarioAdmin(admin.ModelAdmin):
+    readonly_fields = ['slug']
+    inlines = (ColetaFacesInline,)
+
+admin.site.register(Funcionario, FuncionarioAdmin)
+
+
+
+# admin.site.register(Funcionario)
+# admin.site.register(ColetaFaces)
+
+
+admin.site.register(Treinamento)
+

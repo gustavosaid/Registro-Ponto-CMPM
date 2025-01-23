89584bf1dbc4dee7f318d44b63f2c08cf2d807c3
Gustavo GS. Said <gustavosaid@scmpm.local>
2025-01-23 13:05:10 -0300

    Codigo reconhecimento do dia 23/01

--- registro/camera.py
+++ registro/camera.py
@@ -1,55 +1,96 @@
-import cv2 
+import cv2
+import os
 
 class VideoCamera(object):
     def __init__(self):
         # Abre a câmera
         self.face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-        self.video = cv2.VideoCapture(0) #abre a camera do pc 
-        
+        self.video = cv2.VideoCapture(0)  # Abre a câmera do PC
+
         if not self.video.isOpened():
-            print("Erro ao abrir a câmera.")  # Verifica se a câmera foi aberta corretamente
+            print("Erro ao abrir a câmera.")# Verifica se a câmera foi aberta corretamente
+            
+        self.img_dir = "./tmp" #pasta auxiliar 
+        
+        if not os.path.exists(self.img_dir):
+            os.makedirs(self.img_dir) #Cria a pasta 'tmp' se nao existir
 
     def __del__(self):
-        # Libera a câmera ao destruir a classe
-        self.video.release()
+        self.video.release() # Libera a câmera ao destruir a classe
 
     def restart(self):
-        # Reinicia a câmera
-        self.video.release()
+        self.video.release()  # Reinicia a câmera
         self.video = cv2.VideoCapture(0)
 
-    #retorna o frame modo normal do objeto
-    def get_camera(self):
+    # Retorna o frame no modo normal do objeto
+    def get_frame(self):
         # Lê o frame da câmera
         ret, frame = self.video.read()
         if not ret:
             print("Falha ao capturar o frame.")  # Verifica se a captura do frame foi bem-sucedida
-            return None
+            return ret, frame
         
         # Converte o frame em imagem JPEG
         ret, jpeg = cv2.imencode('.jpg', frame)
         if not ret:
             print("Erro ao codificar o frame para JPEG.")  # Verifica se a codificação foi bem-sucedida
-            return None
+            return None, None
         
-        return ret, frame
-    
+        return ret, jpeg.tobytes()
+
     def detect_face(self):
-        ret, frame = self.video.read() #leitura frame
-        if not ret: #verifica se a captura do frame foi bem sucedida
-            print("Falha ao capturar o frame")
+        # Lê o frame da câmera
+        ret, frame = self.video.read() 
+        if not ret:
+            print("Falha ao capturar o frame.")  # Verifica se a captura do frame foi bem-sucedida
             return None
         
-        #conventendo para escala de cinza para melhorar a detecção de faces
-        gray = cv2.cvtColor(frame,cv2.COLOR_BGRGRAY)
+        #Define a regiao de interesse (ROI) onde o rosto sera detectado
+        altura, largura, _ = frame.shape  # Obtém a altura e largura do frame
+        centro_x, centro_y = int(largura/2), int(altura/2)  # Calcula o centro do frame
+        a, b = 140, 180  # Define os valores de a e b (provavelmente para definir uma região de interesse)
+        x1, y1 = centro_x - a, centro_y - b  # Calcula o canto superior esquerdo da região de interesse
+        x2, y2 = centro_x + a, centro_y + b  # Calcula o canto inferior direito da região de interesse
+        roi = frame[y1:y2, x1:x2]  # Cria a região de interesse (ROI)
         
-        #detecta faces do frame
-        faces = self.face_cascade.detectMultoScale(gray,1.3,5)
+        # Converte para escala de cinza para melhorar a detecção de faces
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         
-        #desenho de um retangulo em torno das faces detectadas
-        for (x, y, w, h) in faces_detected:
-            cv2.rectangle(frame, (x, y), (x+w, y+h),(255,0,0),2 )
+        # Detecta faces no frame
+        faces = self.face_cascade.detectMultiScale(gray, 1.3, 5)
         
-        #retorn o frame com a imagem detectada
-        ret,jpeg = cv2.imencode('.jpg',frame)
-        return jpeg,tobytes() #converte o frame para formato jpeg
+        cv2.ellipse(frame,(centro_x,centro_y), (a,b), 0, 0, 360, (0, 0, 255),10) #elipse vermelha
+        
+        
+        
+        # Desenha um retângulo em torno das faces detectadas
+        for (x, y, w, h) in faces:
+            cv2.ellipse(frame,(centro_x,centro_y), (a,b), 0, 0, 360, (0, 255,0 ),10)  #elipese verde
+        
+        # Retorna o frame com as faces detectadas, no formato JPEG
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        return jpeg.tobytes()
+    
+    def sample_faces(self, frame):
+        if frame is None:
+            print("Frame inválido recebido. Abortando detecção.")
+            return None
+
+        # frame = cv2.flip(frame, 0)  # Inverte horizontalmente, ajuste conforme necessário
+        # frame = cv2.resize(frame, (480, 360))  # Redimensiona o frame
+
+        # Detecta as faces no frame
+        faces = self.face_cascade.detectMultiScale(
+            frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30), maxSize=(400, 400)
+        )
+
+        if len(faces) == 0:
+            print("Nenhuma face encontrada.")
+            return None
+
+        for (x, y, w, h) in faces:
+            cropped_face = frame[y:y + h, x:x + w]
+            return cropped_face  # Retorna apenas a primeira face encontrada
+
+        return None
+        
--- registro/views.py
+++ registro/views.py
@@ -1,3 +1,4 @@
+import cv2
 from django.shortcuts import render, redirect
 from .forms import FuncionarioForm, ColetaFacesForm
 from .models import Funcionario, ColetaFaces
@@ -7,18 +8,22 @@ from .camera import VideoCamera
 camera_detection = VideoCamera() #instancia da classe VideoCamera 
 
 #Captura o frame com face detectada
-def gen(camera):
-    while True:
-        frame = camera.get_camera()
-        if frame:
-            break
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+def gen_detect_face(camera):
+     while True:
+        # Obtem o frame da câmera como bytes
+        frame = camera_detection.detect_face()
+        # Se o frame estiver vazio, pule a iteração
+        if frame is None:
+            continue
+        
+        # Monta o streaming HTTP com os bytes do frame
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
         
         
 #cria streming para detecção facial
 def face_detection(request):
-    return StreamingHttpResponse(gen(VideoCamera()),
+    return StreamingHttpResponse(gen_detect_face(camera_detection),
                                  content_type='multipart/x-mixed-replace; boundary=frame')
 
 
@@ -37,13 +42,73 @@ def criar_funcionario(request):
     
 
 def criar_coleta_faces(request, funcionario_id):
-    print(funcionario_id) #identificador o funcionario cadastrado
-    funcionario = Funcionario.objects.get(id=funcionario_id) #resgata funcionario
+    print(funcionario_id)  # Identifica o funcionário cadastrado
+    funcionario = Funcionario.objects.get(id=funcionario_id)  # Resgata o funcionário
     
+    camera_detection = VideoCamera()
+
+    # Corrige a captura do botão clicado
+    botao_clicado = request.POST.get('cliked', 'False') == 'True'
+
     context = {
-        'funcionario': funcionario, #passa o objeto funcionario para o template
-        'face_detection' : face_detection, #pass a camera para renderizar no template
-        #'data_hora': data_hora_atual.strftime('%d/%m/%Y %H:%M:%S'),
+        'funcionario': funcionario,  # Passa o objeto funcionário para o template
+        'face_detection': '/face_detection',  # A URL que lida com o streaming da câmera
+        'valor_botao': botao_clicado,
     }
 
-    return render (request, 'criar_coleta_faces.html', context)
+    if botao_clicado:
+        print("Cliquei em Extrair Imagens !")
+        context = face_extract(context, funcionario,camera_detection)  # Chama função de extrair imagem
+
+    return render(request, 'criar_coleta_faces.html', context)
+
+def face_extract(context, funcionario,camera_detection):
+    num_coletas = ColetaFaces.objects.filter(
+        funcionario__slug=funcionario.slug).count()
+
+    print(num_coletas)  # Quantidade de imagens que o usuário já cadastrou.
+
+    if num_coletas >= 10:
+        context['error'] = 'Limite máximo de coletas atingido.'
+        return context
+
+    amostra = 0  # Amostra inicial
+    numeroAmostras = 10  # Número de amostras para extrair
+    largura, altura = 220, 220  # Dimensão da face (quadrada)
+    file_paths = []  # Lista de caminhos das amostras
+
+    while amostra < numeroAmostras:  # Loop para capturar 10 amostras
+        ret, frame = camera_detection.get_frame()  # Captura o frame da câmera
+
+        # Verifica se o frame foi capturado corretamente
+        if not ret or frame is None:
+            print("Falha ao capturar o frame. Tentando novamente...")
+            continue  # Passa para a próxima iteração do loop
+
+        crop = camera_detection.sample_faces(frame)  # Captura a face no frame
+
+        if crop is not None:  # Se uma face for detectada
+            amostra += 1  # Incrementa o contador de amostras
+
+            # Redimensiona e converte a face para tons de cinza
+            face = cv2.resize(crop, (largura, altura))
+            imagemCinza = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+
+            # Define o caminho para salvar a imagem
+            file_name_path = f'./tmp/{funcionario.slug}_{amostra}.jpg'
+            print(f"Salvando imagem: {file_name_path}")
+
+            cv2.imwrite(file_name_path, imagemCinza)  # Salva a imagem
+            file_paths.append(file_name_path)  # Adiciona à lista de caminhos
+        else:
+            print("Nenhuma face detectada no frame atual.")
+
+        if amostra >= numeroAmostras:
+            break  # Encerra o loop após atingir o número de amostras desejado
+
+    camera_detection.restart()  # Reinicia a câmera após as capturas
+
+    print(f"Arquivos salvos: {file_paths}")
+    return context
+
+            
